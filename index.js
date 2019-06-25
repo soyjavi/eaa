69f534fc01ddc95f4223db8aac4ab0d5f83fa35d
Glitch (hello-express) <none>
2019-06-25 14:20:02 +0000

    🎪🏈 Checkpoint ./index.js:5206337/124 ./views/index.js:5206337/590

--- index.js
+++ index.js
@@ -17,11 +17,9 @@ app.get('/:post', function(req, res) {
   
   fs.readFile(`posts/${post}.md`, 'utf8', function(error, data) {
     if (error) return res.redirect('/');
-    res.send(head + converter.makeHtml(data));
+    res.send(view('index', { content: converter.makeHtml(data) }));
   })
 });
-
-// app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 
 app.get('/', (req, res) => res.send(view('index', { content: 'hola' })));
 
--- views/index.js
+++ views/index.js
@@ -4,11 +4,16 @@ import path from 'path';
 const folder = path.resolve('.', 'views');
 
 export default (filename = 'index', values = {}) => {
-  const uriFile = `${folder}/${filename}.html`;
-  const { rendered: { views: {} } = global;
-  
-  if (!fs.existsSync(uriFile)) throw new Error(`${filename} could not read correctly.`);
-  let view = fs.readFileSync(uriFile, 'utf8');
+  const { renderer: { views } } = global;
+  let view = views[filename];
+
+  if (!view) {
+    const uriFile = `${folder}/${filename}.html`;
+    
+    if (!fs.existsSync(uriFile)) throw new Error(`${filename} could not read correctly.`);
+    view = fs.readFileSync(uriFile, 'utf8');
+    views[filename] = view;
+  }
   
   Object.keys(values).forEach(key => view = view.replace(`{{${key}}}`, values[key]));
   
